924fbc3acbbc4eaaafcb1b63e6d9f167a1ee8fab
Granstation <granstation.130268@gmail.com>
2025-10-01 14:33:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,8 @@ document.getElementById('bookingForm').addEventListener('submit', async function
     try {
         // รองรับหลาย URL (เลือกอันใดอันหนึ่ง)
         //const url = 'https://script.google.com/macros/s/AKfycbzp891sx34BwvBtZhwu76y8fBxCw8J2UvqdMnv7O8-32S0v-uy0RQGKsWY_aqBbyR-sjQ/exec';
-        const url = 'https://script.google.com/macros/s/AKfycbw2lseQDnaim1IHepjfxrXG0NwzpPhcIuiRTeDKd6JKOzKj8a_J5OLZv2R7MlkOPq3duw/exec';
+        // const url = 'https://script.google.com/macros/s/AKfycbw2lseQDnaim1IHepjfxrXG0NwzpPhcIuiRTeDKd6JKOzKj8a_J5OLZv2R7MlkOPq3duw/exec';
+        const url = 'https://script.google.com/macros/s/AKfycbyOsVl_GZMzL6PU0YDjNe5Lt7LOn8rmHfhGgSXsK64-aziVt8cRbMJGyjwn8RQ5bMGR2A/exec';
         const response = await fetch(url, {
             method: 'POST',
             headers: {
@@ -71,4 +72,5 @@ document.getElementById('bookingForm').addEventListener('submit', async function
         loading.style.display = 'none';
     }
 });
+
 
